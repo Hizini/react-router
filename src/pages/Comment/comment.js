59a64429c350a5459120ca67e1ca9e8519f3e659
KimHeeJin <ejfdnrl@naver.com>
2021-11-11 18:39:28 +0900

    [김희진] 11-11 댓글 데이터 서버로 주고 받기 - 댓글 입력 버튼 누를때 axios.post로 서버에 댓글 데이터 전송 - axios.get을 통해 받은 전체 데이터 받아오기

--- src/pages/Comment/comment.js
+++ src/pages/Comment/comment.js
@@ -5,6 +5,7 @@ import './comment.scss';
 import Input from './Input/input';
 import List from './List/list';
 import i18n from "./i18n/i18n";
+import axios from "axios";
 class Comment extends Component {
 
     constructor(props) {
@@ -12,18 +13,47 @@ class Comment extends Component {
         this.state = {
             commentData: [],
             isToggle: false,
-            value: 'kd'
+            value: ''
         }
     }
 
-    getCommentData = (commentValue, nickname, checked) => {
-        let date = new Date()
-        let commentData = this.state.commentData
-        commentData.push({ commentValue, nickname, date, checked })
+    componentDidMount() {
+        this.getCommentData()
+    }
 
-        this.setState({
-            commentData: commentData
-        })
+    inputData = (commentValue, nickname, checked) => {
+        const body = { nickname: checked ? '익명' : nickname, commentValue }
+
+        // let commentData = this.state.commentData
+        // commentData.push({ commentValue, nickname, date, checked })
+
+        // this.setState({
+        //     commentData: commentData
+        // })
+
+        axios.post('http://localhost:2008/api/comment', body)
+            .then(this.getCommentData())
+            .catch(error => {
+                console.log(error)
+                alert('Unknow Error!!')
+            })
+    }
+
+
+    getCommentData = () => {
+
+        axios.get('http://localhost:2008/api/comment')
+            .then(response => {
+                // console.log(response)
+                this.setState({
+                    commentData: response.data.list
+                })
+            })
+            .catch(error => {
+                console.log(error)
+                alert('Unknown Error!')
+
+            })
 
     }
 
@@ -31,22 +61,20 @@ class Comment extends Component {
         this.setState({
             isToggle: !this.state.isToggle
         })
-        console.log(this.state.isToggle)
     }
 
     changeLanguage = (e) => {
         this.setState({
-            value : e.target.value
+            value: e.target.value
         })
         i18n.changeLanguage(e.target.value)
     }
 
     render() {
-        console.log(this.state.commentData)
         return (
             <div className="comment-container">
                 <div className="Comment">
-                    <button onClick={this.onClickButtonHandler} className="button">{this.state.isToggle ? `${i18n.t('commentDisable')}`: `${i18n.t('commmentActivation')}`}</button>
+                    <button onClick={this.onClickButtonHandler} className="button">{this.state.isToggle ? `${i18n.t('commentDisable')}` : `${i18n.t('commmentActivation')}`}</button>
                     <select onChange={this.changeLanguage} value={this.state.value} className="Dropbox">
                         <option value="en">American</option>
                         <option value="ko" selected>한국어</option>
@@ -55,7 +83,7 @@ class Comment extends Component {
                     </select>
                     <div className="List">
                         <List listData={this.state.commentData} />
-                        {this.state.isToggle ? <Input inputData={this.getCommentData} /> : ''}
+                        {this.state.isToggle ? <Input inputData={this.inputData} /> : ''}
                     </div>
                 </div>
             </div>
